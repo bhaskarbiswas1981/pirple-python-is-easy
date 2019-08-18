6ee236d391c09117d10e114c5e6b0260807b6153
Bhaskar Biswas <bhaskarbiswas1981@gmail.com>
2019-08-18 21:25:31 +0530

    This file is part of Homework #1:variables

---
+++ main.py
@@ -0,0 +1,30 @@
+'''
+This multi-line comments.
+'''
+#Songs Meta-data variables
+song_name="Big Yellow Taxi (Joni Mitchell cover)"
+artist="Joni Mitchell"
+composer="Joni Mitchell"
+year=1970
+genre="Folk rock; pop; folk;"
+BPM=126
+grouping="Earthprogram Music(one-stop)"
+comments="Contact info for licensing party"
+album="LyricFiiind"
+album_artist=""
+disc_number=0
+track=0
+
+#Prnt all the songs meta-data
+print(song_name)
+print(artist)
+print(composer)
+print(year)
+print(genre)
+print(BPM)
+print(grouping)
+print(comments)
+print(album)
+print(album_artist)
+print(disc_number)
+print(track)
